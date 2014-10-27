6f004d384868251e554e07c0c7fe81830638550a
Ryan Wiemer <ryan@ryanwiemer.com>
2014-10-27 01:13:15 -0700

    adding icons fonts to sidebar

--- sidebar.php
+++ sidebar.php
@@ -10,10 +10,10 @@
 </div>
   <h3 class="sidebar__title">Follow K&amp;R Blog</h3>
     <div class="sidebar__social">
-      <a href="#"><span class="icon-twitter"></span></a>
-      <a href="#"><span class="icon-instagram"></span></a>
-      <a href="#"><span class="icon-pinterest"></span></a>
-      <a href="#"><span class="icon-twitter"></span></a>
+      <a href="https://twitter.com/Kirstennoellew"><span class="icon-twitter"></span></a>
+      <a href="http://instagram.com/kirstennoellew"><span class="icon-instagram"></span></a>
+      <a href="http://www.pinterest.com/kirstennoellew/"><span class="icon-pinterest"></span></a>
+      <a href="http://www.bloglovin.com/en/blog/3862566"><span class="icon-bloglovin"></span></a>
     </div>
 
     <div class="sidebar__archives">
