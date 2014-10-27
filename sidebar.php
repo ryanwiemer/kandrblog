d2dbc7f612b04cd726eae64bff280fbc6aebc4d6
Ryan Wiemer <ryan@ryanwiemer.com>
2014-10-26 18:59:25 -0700

    working on sidebar

--- sidebar.php
+++ sidebar.php
@@ -8,10 +8,10 @@
   <div class="sidebar__img"></div>
   <h3 class="sidebar__title">Follow K&amp;R Blog<h3/>
     <div class="sidebar__social">
-      <a href="#"><span class="icon-twitter"></span> Twitter </a>
-      <a href="#"><span class="icon-instagram"></span> Instagram</a>
-      <a href="#"><span class="icon-pinterest"></span> Pinterest</a>
-      <a href="#"><span class="icon-twitter"></span> Bloglovin'</a>
+      <a href="#"><span class="icon-twitter"></span></a>
+      <a href="#"><span class="icon-instagram"></span></a>
+      <a href="#"><span class="icon-pinterest"></span></a>
+      <a href="#"><span class="icon-twitter"></span></a>
     </div>
 </div><!-- .sidebar
 
