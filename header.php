e14003139d351239399c83657308c2a43a6a659d
Ryan Wiemer <ryan@ryanwiemer.com>
2014-11-13 23:31:59 -0800

    removing mobile fixed position fix as it didn't work properly

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 ?><!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" />>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
   <title><?php wp_title('|', true, 'right'); ?></title>
   <script src="//use.typekit.net/fqx1jej.js"></script>
